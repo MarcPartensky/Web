f48da02ad97fd330d5eef1dc93ec0695d08ecccd
Marc Partensky <marc.partensky@gmail.com>
2020-01-17 17:54:34 +0100

    done nothing special

--- Javascript/Canvas/Ball/matrix.js
+++ Javascript/Canvas/Ball/matrix.js
@@ -1,9 +1,26 @@
 class Matrix {
-  constructor(...args) {
-    if (args[0] instanceof Array && args.length==1) {
-      this.array = args[0];
-    } else {
-      this.array = Array(...args);
+  /*
+  * A matrix can be built from a 2d array, a list of vectors
+  */
+  static fromVectors( ...vectors) {
+    let x = Math.max(vectors.map());
+    let h = vectors.length
+    return new Matrix(array);
+  }
+  get format() {
+    return [this.array.length, this.array[0].length];
+  }
+
+  constructor(args) {
+    this.array = Array(...args);
+  }
+  dot() {
+
+  }
+  mul(matrix) {
+    let m = new Matrix()
+    for (let ) {
+
     }
   }
 
