54743d983eaac13465fe9622ad3b5f455f8862b3
Marc Partensky <marc.partensky@gmail.com>
2020-01-17 19:00:07 +0100

    test

--- Javascript/Canvas/Ball/matrix.js
+++ Javascript/Canvas/Ball/matrix.js
@@ -30,15 +30,15 @@ class Matrix {
 
   }
   mul(matrix) {
-    let w = this.width;
-    let d = this.height;
-    let h = matrix.height;
-    let m = new Matrix.fromFormat(w, h);
+    let d = this.width;
+    let h = this.height;
+    let w = matrix.width;
+    let m = Matrix.fromFormat(w, h);
     for (let x=0; x<w; x++) {
       for (let y=0; y<h; y++) {
         m.array[x][y] = 0;
-        for (let z=0; z<d) {
-          m.array[x][y] += matrix.array[x][z]*this.array[z][y];
+        for (let z=0; z<d; z++) {
+          m.array[x][y] += this.array[z][y]*matrix.array[x][z];
         }
       }
     }
