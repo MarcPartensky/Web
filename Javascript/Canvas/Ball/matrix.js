26b929344d71fd9c08c9743bbf26e3ab09c86de4
Marc Partensky <marc.partensky@gmail.com>
2020-01-17 18:50:02 +0100

    matrix news

--- Javascript/Canvas/Ball/matrix.js
+++ Javascript/Canvas/Ball/matrix.js
@@ -2,15 +2,27 @@ class Matrix {
   /*
   * A matrix can be built from a 2d array, a list of vectors
   */
+  static fromFormat(width, height, value=undefined) {
+    let array = new Array(width);
+    for (let x=0; x<width; x++) {
+      array[x] = new Array(height);
+    }
+    return new Matrix(array);
+  }
   static fromVectors( ...vectors) {
     let x = Math.max(vectors.map());
     let h = vectors.length
     return new Matrix(array);
   }
-  get format() {
-    return [this.array.length, this.array[0].length];
+  get width() {
+    return this.array.length;
   }
-
+  get height() {
+    return this.array[0].length;
+  }
+  get format() {
+    return [this.width, this.height;
+  }
   constructor(args) {
     this.array = Array(...args);
   }
@@ -18,10 +30,18 @@ class Matrix {
 
   }
   mul(matrix) {
-    let m = new Matrix()
-    for (let ) {
-
+    let w = this.width;
+    let d = this.height;
+    let h = matrix.height;
+    let m = new Matrix.fromFormat(w, h);
+    for (let x=0; x<w; x++) {
+      for (let y=0; y<h; y++) {
+        m.array[x][y] = 0;
+        for (let z=0; z<d) {
+          m.array[x][y] += matrix.array[x][z]*this.array[z][y];
+        }
+      }
     }
+    return m;
   }
-
 }
