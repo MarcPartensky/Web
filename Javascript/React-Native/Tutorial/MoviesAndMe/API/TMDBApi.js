18d36cb911caf7ea8152ce5356b5c7ad04910434
Marc Partensky <marc.partensky@gmail.com>
2020-11-07 23:41:47 +0100

    --version

--- Javascript/React-Native/Tutorial/MoviesAndMe/API/TMDBApi.js
+++ Javascript/React-Native/Tutorial/MoviesAndMe/API/TMDBApi.js
@@ -1,9 +1,8 @@
-import API_TOKEN from '../Helpers/token'
-const API_TOKEN = "9bcddcc1dcaa971161ee8474d0034667"
+import { API_TOKEN } from '../Helpers/token'
 
-export function getFilmsFromApiWithSearchedText(text) {
-    const url = "https://api.themoviedb.org/3/search/movie?api_key=" + API_TOKEN + "&language=fr&query=" + text
-    return fetch(url)
-        .then((response) => response.json())
-        .catch((error) => console.log(error))
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
 }
--- Javascript/React-Native/Tutorial/MoviesAndMe/components/Search.js
+++ Javascript/React-Native/Tutorial/MoviesAndMe/components/Search.js
@@ -1,22 +1,40 @@
 import React from 'react'
-import { View, Button, TextInput, StyleSheet } from 'react-native'
+import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
+import{ getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 export default class Search extends React.Component {
-    _loadFilms() {
 
+    // constructor(props) {
+    //     super(props)
+    //     this.state = { films: [] }
+    // }
+    _loadFilms() {
+        getFilmsFromApiWithSearchedText("star").then(data => console.log(data))
     }
 
     render() {
         return (
-            <View style={{ flex: 1, backgroundcolor: 'yellow', flexDirection: 'row' }}>
-                <View style={{ flex: 1, backgroundColor: 'red' }}></View>
-                <View style={{ flex: 2, backgroundColor: 'green' }}></View>
-            </View>
+          <View style={styles.main_container}>
+            <TextInput style={styles.textinput} placeholder='Titre du film'/>
+              <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+            {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
+            <FlatList
+                data={films}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) => <FilmItem film={item}/>}
+            />
+          </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    main_container: {
+        flex: 1,
+        marginTop: 20
+    },
     textinput: {
         marginLeft: 5,
         marginRight: 5,
