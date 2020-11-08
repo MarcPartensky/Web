c744123e4da7fbca15c8bfb9550ee0349e0ff2ab
Marc Partensky <marc.partensky@gmail.com>
2020-11-08 01:06:33 +0100

    movies search done

--- Javascript/React-Native/Tutorial/MoviesAndMe/components/FilmItem.js
+++ Javascript/React-Native/Tutorial/MoviesAndMe/components/FilmItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
+import { getImageFromApi } from '../API/TMDBApi'
 
 class FilmItem extends React.Component {
     // Components/FilmItem.js
@@ -8,8 +9,8 @@ class FilmItem extends React.Component {
         return (
           <View style={styles.main_container}>
             <Image
-              style={styles.image}
-              source={{uri: "image"}}
+                style={styles.image}
+                source={{uri: getImageFromApi(film.poster_path)}}
             />
             <View style={styles.content_container}>
               <View style={styles.header_container}>
--- Javascript/React-Native/Tutorial/MoviesAndMe/components/Search.js
+++ Javascript/React-Native/Tutorial/MoviesAndMe/components/Search.js
@@ -1,30 +1,64 @@
 import React from 'react'
-import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
+import { StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator } from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 import{ getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 export default class Search extends React.Component {
 
-    // constructor(props) {
-    //     super(props)
-    //     this.state = { films: [] }
-    // }
-    _loadFilms() {
-        getFilmsFromApiWithSearchedText("star").then(data => console.log(data))
+    constructor(props) {
+        super(props)
+        this.state = {
+            films: [],
+            isLoading: false,
+            searchedText: ""
+        }
     }
+
+    _loadFilms() {
+        this.setState({ isLoading: true })
+        if (this.state.searchedText.length > 0) {
+            getFilmsFromApiWithSearchedText(this.state.searchedText).then(data => {
+                this.setState({
+                    films: data.results,
+                    isLoading: false
+                })
+            })
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        this.setState({ searchedText: text })
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size="large" />
+                </View>
+            )
+        }
+    }
+
 
     render() {
         return (
           <View style={styles.main_container}>
-            <TextInput style={styles.textinput} placeholder='Titre du film'/>
+            <TextInput
+              style={styles.textinput}
+              placeholder='Titre du film'
+              onChangeText={(text) => this._searchTextInputChanged(text)}
+              onSubmitEditing={() => this._loadFilms()}
+            />
               <Button title='Rechercher' onPress={() => this._loadFilms()}/>
             {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
             <FlatList
-                data={films}
+                data={this.state.films}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({item}) => <FilmItem film={item}/>}
             />
+            {this._displayLoading()}
           </View>
         )
     }
@@ -42,5 +76,14 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
