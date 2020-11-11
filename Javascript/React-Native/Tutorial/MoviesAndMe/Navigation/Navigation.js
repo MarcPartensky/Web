9fae5bdab5e97eb8354def8f1369dbbd49776ca7
Marc Partensky <marc.partensky@gmail.com>
2020-11-11 18:57:13 +0100

    expo installs for navigation

--- Javascript/React-Native/Tutorial/MoviesAndMe/Navigation/Navigation.js
+++ Javascript/React-Native/Tutorial/MoviesAndMe/Navigation/Navigation.js
@@ -1,4 +1,5 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 import Search from '../Components/Search'
 import FilmDetail from '../Components/FilmDetail'
 
